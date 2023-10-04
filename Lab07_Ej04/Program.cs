840ac771eb6fd2b437bf2757dcdd37af9ce73d2f
Sandra Carolina <Sandra Carolina@DESKTOP-SFK7P55>
2023-10-03 21:40:58 -0500

    Agregar archivos de proyecto.

---
+++ Lab07_Ej04/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07_Ej04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            do
+            {
+                Console.Write("Ingresa un número: ");
+                numero = int.Parse(Console.ReadLine());
+             
+                if (numero > 0)
+                {
+                    for (int i = 1; i <= numero; i++)
+                    {
+                        for (int j = 1; j <= numero * 2; j++)
+                        {
+                            Console.Write("*");
+                        }
+                        Console.WriteLine();
+                    }
+                }              
+            } while (numero != 0);
+        }              
+    }
+}
